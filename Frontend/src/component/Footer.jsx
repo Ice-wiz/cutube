23f95f7f13f47e085d264374af31457efa41eaed
Aryan <img_2020012@iiitm.ac.in>
2024-07-04 12:28:35 +0530

    Update Footer.jsx

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white py-6">
       <div className="max-w-screen-lg mx-auto text-center">
         <p className="text-lg mb-4">
-          Enjoy seamless communication with our custom email solution.
+          Enjoy seamless uploads with our cutube video based social app.
         </p>
         <p className="text-sm">
-          Made by Aryan, powered by React.js and Tailwind CSS.
+          Made by Aryan, powered by React.js and Tailwind CSS and node 
         </p>
       </div>
     </footer>
